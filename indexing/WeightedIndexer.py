0b92b38dca100d38b6a42dbf9ae463ba899669f5
Alina <alina-yanchuk02@outlook.pt>
2020-12-27 16:50:15 +0000

    posições no lnc

--- indexing/WeightedIndexer.py
+++ indexing/WeightedIndexer.py
@@ -53,10 +53,11 @@ class WeightedIndexer:
                 doc_pow_sum[doc_id] += weight ** 2 # sum of all the weights of each document
                                                    # each weight to the pow of 2
                                                    # this will be used in the cossine normalization
+                docsWeigh[doc_id][1] = self.inverted_index[term][1][doc_id] # list with term positions in this doc
             # normalization - cossine normalization:
             # the cossine normalization is sqrt the inverse of the sum of all the weights of a document, each one to the pow of 2
             for doc_id in doc_pow_sum:
-                docsWeigh[doc_id] = 1 / math.sqrt(doc_pow_sum[doc_id])
+                docsWeigh[doc_id][0] = 1 / math.sqrt(doc_pow_sum[doc_id])
                         
             idf_docsWeight.append(docsWeigh)
 
